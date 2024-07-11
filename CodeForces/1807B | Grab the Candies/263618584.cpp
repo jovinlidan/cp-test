f95bdcb005b0a2ab587fe8af036716ea57f7f7c7
Jovin Lidan <jovinlidan1@gmail.com>
2024-07-11 20:33:45 +0700

    Add solution for 1807

---
+++ CodeForces/1807B | Grab the Candies/263618584.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF -1e9
+typedef long long int ll;
+typedef pair <int,int> pii;
+typedef pair <pii,int> piii;
+const ll MOD = 1e9+7;
+const int mxN = 1000005;
+int dx[4] = {0,0,1,-1};
+int dy[4] = {1,-1,0,0};
+int kx[8] = { 2, 1, -1, -2, -2, -1, 1, 2};
+int ky[8] = { 1, 2, 2, 1, -1, -2, -2, -1};
+ll qpow(unsigned long long x , int y , int p)
+{
+    ll res = 1;
+    x = x % p;
+    while (y > 0)
+    {
+        if (y % 2 == 1) res = (res * x) % p;
+        y = y / 2;
+        x = (x * x) % p;
+    }
+    return res;
+}
+ll modInverse(unsigned long long x , int p)
+{
+    return qpow(x, p - 2 , p);
+}
+double calc (ll t1 , ll t2 , ll y1 , ll y2)
+{
+    return (t1 * y1 + t2 * y2) * 1.0 /(y1 + y2);
+}
+
+string getBit(int n){
+    string s = "";
+    while(n){
+        s += (n % 2) + '0';
+        n /= 2;
+    }
+    reverse(s.begin(), s.end());
+    return s;
+}
+
+vector<ll> returnBit(ll xx){
+    vector<ll> ret;
+    for(int i = 0; i < 31; i++){
+        if(xx & (1ll << i)){
+            ret.push_back(1);
+        }
+        else{
+            ret.push_back(0);
+        }
+    }
+    return ret;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc;
+    cin >> tc;
+
+    while(tc--){
+        int n ;
+        cin >> n;
+        vector<int> v(n);
+        int total = 0;
+        for(int i = 0; i < n; i++){
+            cin >> v[i];
+            total += v[i];
+        }
+
+        sort(v.begin(), v.end(), greater<int>());
+
+        bool flag = true;
+        int m= 0, b = 0;
+        for(int i = 0 ;i < n; i++){
+            if(v[i] % 2 == 0){
+                m+=v[i];
+            }
+           
+        }
+        if((total - m) >= m){
+            flag = false;
+        }
+        if(flag)cout << "YES\n";
+        else cout << "NO\n";
+        
+
+        
+    }
+    
+}
