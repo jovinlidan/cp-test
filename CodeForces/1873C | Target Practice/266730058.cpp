303ac426a77eb68d1a5123474bf8f41e8c20a47c
Jovin Lidan <jovinlidan1@gmail.com>
2024-07-11 20:11:14 +0700

    Add solution for 1873

---
+++ CodeForces/1873C | Target Practice/266730058.cpp
@@ -0,0 +1,128 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF -1e9
+typedef long long int ll;
+typedef pair <int,int> pii;
+typedef pair <pii,int> piii;
+const ll MOD = 1e9+7;
+const int mxN = 1000005;
+int dx[4] = {0,0,1,-1};
+int dy[4] = {1,-1,0,0};
+int kx[8] = { 2, 1, -1, -2, -2, -1, 1, 2};
+int ky[8] = { 1, 2, 2, 1, -1, -2, -2, -1};
+ll qpow(unsigned long long x , int y , int p)
+{
+    ll res = 1;
+    x = x % p;
+    while (y > 0)
+    {
+        if (y % 2 == 1) res = (res * x) % p;
+        y = y / 2;
+        x = (x * x) % p;
+    }
+    return res;
+}
+ll modInverse(unsigned long long x , int p)
+{
+    return qpow(x, p - 2 , p);
+}
+double calc (ll t1 , ll t2 , ll y1 , ll y2)
+{
+    return (t1 * y1 + t2 * y2) * 1.0 /(y1 + y2);
+}
+
+string getBit(int n){
+    string s = "";
+    while(n){
+        s += (n % 2) + '0';
+        n /= 2;
+    }
+    reverse(s.begin(), s.end());
+    return s;
+}
+
+vector<ll> returnBit(ll xx){
+    vector<ll> ret;
+    for(int i = 0; i < 31; i++){
+        if(xx & (1ll << i)){
+            ret.push_back(1);
+        }
+        else{
+            ret.push_back(0);
+        }
+    }
+    return ret;
+}
+
+int gcdFunc(int a, int b) 
+{ 
+    // Everything divides 0 
+    if (a == 0) 
+        return b; 
+    if (b == 0) 
+        return a; 
+  
+    // base case 
+    if (a == b) 
+        return a; 
+  
+    // a is greater 
+    if (a > b) 
+        return gcd(a - b, b); 
+    return gcd(a, b - a); 
+} 
+
+ll sn(ll n, ll a,ll b){
+    return n/2.0 * (2*a + (n-1)*b);
+}
+
+void solve(){
+    char x;
+    int ans = 0;
+    for(int i = 0;i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            cin >> x;
+            if(x != 'X')continue;
+            int point = 0;
+            int ii = i, jj = j;
+            if( ii > 5)ii = 10 - ii - 1;
+            if( jj > 5)jj = 10 - jj - 1;
+            int val = min(ii, jj);
+
+            // if(i > 5 || j > 5)val = max(i, j);
+
+            if(i == 5 && j == 5){
+                point = 5;
+            }
+            else {
+                if(val >= 6) {
+                    point = 10 - val;
+                }else {
+                    point = val + 1;
+                }
+            }
+            ans += point;
+            // cout << point << " ";
+        }
+        // cout << '\n';
+    }
+    cout << ans << '\n';
+
+
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc;
+    cin >> tc;
+
+    while(tc--){
+        solve();
+    }
+    
+}
