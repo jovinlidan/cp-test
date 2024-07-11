cc287bfefa74df07f0f193e55771e3e153666ca9
Jovin Lidan <jovinlidan1@gmail.com>
2024-07-11 20:34:05 +0700

    Add solution for 1976

---
+++ CodeForces/1976A | Verify Password/263269504.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF -1e9
+typedef long long int ll;
+typedef pair <int,int> pii;
+typedef pair <pii,int> piii;
+const ll MOD = 1e9+7;
+const int mxN = 1000005;
+int dx[4] = {0,0,1,-1};
+int dy[4] = {1,-1,0,0};
+int kx[8] = { 2, 1, -1, -2, -2, -1, 1, 2};
+int ky[8] = { 1, 2, 2, 1, -1, -2, -2, -1};
+ll qpow(unsigned long long x , int y , int p)
+{
+    ll res = 1;
+    x = x % p;
+    while (y > 0)
+    {
+        if (y % 2 == 1) res = (res * x) % p;
+        y = y / 2;
+        x = (x * x) % p;
+    }
+    return res;
+}
+ll modInverse(unsigned long long x , int p)
+{
+    return qpow(x, p - 2 , p);
+}
+double calc (ll t1 , ll t2 , ll y1 , ll y2)
+{
+    return (t1 * y1 + t2 * y2) * 1.0 /(y1 + y2);
+}
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc;
+    cin >> tc;
+    while(tc--){
+        int n;
+        cin >> n;
+        string s, ss;
+        cin >> s;
+        ss =s;
+
+        sort(s.begin(), s.end());
+        if(ss == s){
+            cout << "YES\n";
+        }
+        else {
+            cout << "NO\n";
+        }
+
+    }
+    
+}
