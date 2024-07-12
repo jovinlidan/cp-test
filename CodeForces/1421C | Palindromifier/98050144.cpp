66d04627a8fe39e256ce9af2502635f175baab72
Jovin Lidan <jovinlidan1@gmail.com>
2024-07-12 13:48:28 +0700

    Add solution for 1421

---
+++ CodeForces/1421C | Palindromifier/98050144.cpp
@@ -0,0 +1,128 @@
+#include <bits/stdc++.h>
+using namespace std;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+#define mxN 10000005
+#define INF -1e9
+typedef pair<int, int> pii;
+
+typedef long long int ll;
+const int mod = 1e9+7;
+
+ll modpow(ll x, ll y, ll m)
+{
+    ll res = 1;
+    while(y > 0)
+    {
+        if(y & 1)
+            res = (res * x) % m;
+        x = (x * x) % m;
+        y >>= 1;
+    }
+
+    return res;
+}
+
+bool cmp(pii l, pii r)
+{
+    if(l.first == r.first)
+    {
+        return l.second > r.second;
+    }
+    return l.first < r.first;
+}
+
+//void sieve()
+//{
+//    vector<bool> prime(mxN, true);
+//    int n = mxN;
+//    prime[0] = prime[1] = false;
+//    for (int i = 2; i <= n; i++) {
+//        if (prime[i] && (long long)i * i <= n) {
+//            for (int j = i * i; j <= n; j += i)
+//                prime[j] = false;
+//        }
+//    }
+//    primeList.push_back(2);
+//    for(int j = 3 ; j <= mxN ; j+=2)
+//    {
+//        if (prime[j])
+//        {
+//            isPrime[j] = 1;
+//        }
+//    }
+//}
+
+bool checkPal(string s)
+{
+    int l = 0, r = s.length()-1;
+    while(l < r)
+    {
+        if(s[l] != s[r])return 0;
+        l++;
+        r--;
+    }
+    return 1;
+}
+int main()
+{
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+
+
+
+
+
+    int tc = 1;
+   // cin>>tc;
+    while(tc--)
+    {
+        string s;
+        cin >> s;
+        if(checkPal(s))
+        {
+            cout << 0 << '\n';
+            return 0;
+        }
+        int n = s.length();
+        cout << 3 << '\n';
+        cout << "R " << n-1<<'\n';
+        n++;
+        cout << "L " << n-1<<'\n';
+        cout << "L " << 2<<'\n';
+        //cout << "L " << 2<<'\n';
+
+
+    }
+
+
+    return 0;
+}
